11d20462c2e018e54664a4c934e3acbae2afa1e1
Your Name <you@example.com>
2020-03-15 20:13:22 -0400

    success

--- js/main.js
+++ js/main.js
@@ -5,10 +5,19 @@
 /**
  * Sample code
  */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
+//document.addEventListener('DOMContentLoaded', function() {
+//  var ulElement = document.getElementById('links');
+//  var liElement = document.createElement('li');
+//  liElement.appendChild(document.createTextNode('created by main.js'));
 
-  ulElement.appendChild(liElement);
-});
+//  ulElement.appendChild(liElement);
+//});
+
+$.getJSON("https://api.fda.gov/food/enforcement.json?search=distribution_pattern:%22nationwide%22&limit=5",
+    function (data) {
+        console.log(data);
+
+        var country = data.country;
+
+        $(".c2").append(country);
+    });
